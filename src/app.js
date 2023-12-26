7be4a20a9e2b82f0da149f699f3d6f9f1ed9d229
Ashish Raj <21c47@bcebakhtiyarpur.org>
2023-12-26 15:26:37 +0530

    all config. done and routes tested

--- src/app.js
+++ src/app.js
@@ -22,4 +22,14 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+
+// routes
+import userRouter from "./routes/user.routes.js";
+
+//routes declare
+app.use("/api/v1/users" , userRouter )
+
+//URL :- http://localhost:8000/api/v1/users/register
+
+
 export { app };
