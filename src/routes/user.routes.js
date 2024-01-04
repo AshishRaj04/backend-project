862ba03267be4a1fe887a143187d06bfe361d7b4
Ashish Raj <21c47@bcebakhtiyarpur.org>
2024-01-04 20:34:19 +0530

    add new routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,4 +1,3 @@
-// Import necessary dependencies
 import { Router } from "express";
 import {
   registerUser,
@@ -9,21 +8,14 @@ import {
   currentUser,
   updateAvatar,
   updateCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import verifyJWT from "../middlewares/auth.middleware.js";
 
-// Initialize the router
 const router = Router();
 
-/**
- * Route to register a new user.
- *
- * This route uses multer middleware to handle file uploads. It uploads the avatar and coverImage files from the request.
- * The upload middleware limits the number of files for each field to 1.
- *
- * The request is then passed to the registerUser function from the user.controller.js file.
- */
 router.route("/register").post(
   upload.fields([
     { name: "avatar", maxCount: 1 },
@@ -32,19 +24,8 @@ router.route("/register").post(
   registerUser
 );
 
-/**
- * Route to log in a user.
- *
- * The request is passed to the loginUser function from the user.controller.js file.
- */
 router.route("/login").post(loginUser);
 
-/**
- * Route to log out a user.
- *
- * This route uses the verifyJWT middleware to verify if the JWT token in the request is valid.
- * If the token is valid, the request is passed to the logoutUser function from the user.controller.js file.
- */
 router.route("/logout").post(verifyJWT, logoutUser);
 
 router.route("/refreshToken").get(refreshAccessToken);
@@ -53,18 +34,11 @@ router.route("/changePassword").post(verifyJWT, changePassword);
 router.route("/getCurrentUser").post(verifyJWT, currentUser);
 router
   .route("/updateUserAvatar")
-  .post(
-    verifyJWT,
-    upload.single("avatar"),
-    updateAvatar
-  );
+  .patch(verifyJWT, upload.single("avatar"), updateAvatar);
 router
   .route("/updateUserCoverImage")
-  .post(
-    verifyJWT,
-    upload.single("coverImage"),
-    updateCoverImage
-  );
+  .patch(verifyJWT, upload.single("coverImage"), updateCoverImage);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/watchHistory").get(verifyJWT, getWatchHistory);
 
-// Export the router
 export default router;
